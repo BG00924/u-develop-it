23f05292bbf2921d4e722a7b8ddd10dcbc5e88fb
BG00924 <BG00924@users.noreply.github.com>
2020-09-12 18:54:47 -0500

    add post route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
 const sqlite3 = require('sqlite3').verbose()
+const inputCheck = require('./utils/inputCheck');
 
 //express middleware
 app.use(express.urlencoded({ extended: false}))
@@ -95,6 +96,29 @@ app.delete('/api/candidate/:id', (req, res) => {
 //     }
 //     console.log(result, this.lastID)
 // })
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 // Default response for any other request(Not Found) Catch all; needs to be last as it will override others
 app.use((req, res) => {
